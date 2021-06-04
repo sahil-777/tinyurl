0c201e5f8c7549a795eaec191ef0a482f90af826
sahil-777 <turbothunderboltz@gmail.com>
2021-06-05 03:30:38 +0530

    minor change

--- public/js/index.js
+++ public/js/index.js
@@ -17,7 +17,6 @@ function getShortUrl() {
         }
     })
 }
-//    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
 function goToUrl() {
     let url = document.getElementById('input-output').value;
