c079ce895fdfe202d7a77ca47843c79aea358f8f
sahil-777 <turbothunderboltz@gmail.com>
2021-06-18 02:36:44 +0530

    getLongUrl lambda function completed

--- api/AWS-Lambda Functions/getLongUrl/index.js
+++ api/AWS-Lambda Functions/getLongUrl/index.js
@@ -2,29 +2,51 @@
 // e.g. : https://99dk65tgz5.execute-api.us-east-1.amazonaws.com/api/getlongurl/6264a30
 
 const AWS = require('aws-sdk');
-const ddb = new AWS.DynamoDB.DocumentClient({
+const docClient = new AWS.DynamoDB.DocumentClient({
     region: 'us-east-1'
 });
 
-exports.handler = async (event, context, callback) => {
-    let shortKey = event.pathParameters.shortkey;
-    await fetchUrl(shortKey).then(data => {
-        callback(null, {
-            statusCode: 200,
-            body: JSON.stringify(data)
-        });
-    }).catch((err) => {
-        console.error(err);
-    });
-};
-
-function fetchUrl(shortKey) {
-    const params = {
-        TableName: 'urlMapper',
+async function fetchUrl(shortKey) {
+    if (shortKey == 'favicon.ico' || shortKey == '') {
+        return 0; //return domain Address
+    }
+    let params = {
+        TableName: "urlMapper",
         Key: {
             "shortURLkey": shortKey
         }
     };
-    return ddb.get(params).promise();
+    let response;
+    try {
+        response = await docClient.get(params).promise();
+    } catch (error) {
+        console.log(error.code);
+        console.log(error.statusCode);
+        return {
+            statusCode: 500,
+            longUrl: 'NA',
+            msg: 'Internal Server Error, Please try again later'
+        };
+    }
+    if (!response || !response.Item) {
+        return {
+            statusCode: 404,
+            longUrl: 'NA',
+            msg: 'Page not found'
+        };
+    } else {
+        return {
+            statusCode: 200,
+            longUrl: response.Item.longURL,
+            msg: 'Everything is working, sucessfully'
+        };
+    }
 }
 
+exports.handler = async (event, context, callback) => {
+    let shortKey = event.pathParameters.shortkey;
+    let responce = await fetchUrl(shortKey);
+    return {
+        body: JSON.stringify(responce)
+    };
+};
