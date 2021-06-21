9607033def51e7161344fe5ede2dfc84987d9bdc
sahil-777 <turbothunderboltz@gmail.com>
2021-06-22 01:13:54 +0530

    Query-parameter added

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,39 +1,49 @@
+async function initiate() {
+    //let shortKey=location.href.split('/').pop().split('?').pop()
+    let shortKey = location.href.split('?')[1]
+    console.log(shortKey)
+    if (!shortKey) {
+        return
+    }
+
+    goToUrl(location.href)
+}
+
 async function getShortUrl() {
     let longUrl = document.getElementById('input-output').value
 
     if (isURLValid(longUrl)) { //check if url is valid or not
         let response;
         try {
-            response=await axios({
+            response = await axios({
                 method: 'post',
                 url: 'https://99dk65tgz5.execute-api.us-east-1.amazonaws.com/api/getshorturl',
                 data: {
-                  longURL: longUrl
+                    longURL: longUrl
                 }
-              });            
+            });
         } catch (error) {
-                document.getElementById('error-msg').innerText = errorThrown + '. Please, try again later'
-                setTimeout(() => {
-                    document.getElementById('error-msg').innerText = '';
-                }, 2500)
-                return
+            document.getElementById('error-msg').innerText = errorThrown + '. Please, try again later'
+            setTimeout(() => {
+                document.getElementById('error-msg').innerText = '';
+            }, 2500)
+            return
         }
-        response=response.data
-        if(response.statusCode==201){
+        response = response.data
+        if (response.statusCode == 201) {
             let shortKey = response.shortKey;
-            let shortURL = window.location.href+shortKey // Add '/', if u r running locally
+            let shortURL = window.location.href + '?' + shortKey // Add '/', if u r running locally
             document.getElementById('input-output').value = shortURL
             document.getElementById('copy-btn').hidden = false
             document.getElementById('shorten-btn').hidden = true
             document.getElementById('check-btn').hidden = false
             document.getElementById('shorten-another-btn').hidden = false
-        }
-        else{
+        } else {
             let errorMsg = response.msg
             document.getElementById('error-msg').innerText = errorMsg + '. Please, try again later'
             setTimeout(() => {
                 document.getElementById('error-msg').innerText = '';
-            }, 2500)   
+            }, 2500)
         }
     } else {
         document.getElementById('error-msg').innerText = 'Please,enter valid url'
@@ -43,9 +53,31 @@ async function getShortUrl() {
     }
 }
 
-function goToUrl() {
-    let url = document.getElementById('input-output').value;
-    window.open(url) //will be opened in new tab
+async function getLongURL(shortURL) {
+    //let url = document.getElementById('input-output').value;
+    //window.open(longURL) //will be opened in new tab
+    let mainAddress = shortURL.split('?')[0]
+    let shortKey = shortURL.split('?')[1]
+    //console.log(shortKey)
+    let longURL = ''
+    try {
+        let response = await axios.get('https://99dk65tgz5.execute-api.us-east-1.amazonaws.com/api/getlongurl/' + shortKey)
+        if (response.data.statusCode == 200)
+            longURL = response.data.longUrl;
+        else {
+            longURL = mainAddress + 'error-page.html'
+            return
+        }
+    } catch (error) {
+        longURL = mainAddress + 'error-page.html'
+        return
+    }
+    return longURL;
+}
+
+async function goToUrl(shortURL) {
+    let longURL = await getLongURL(shortURL)
+    window.open(longURL)
 }
 
 function copyToClipboard(element) {
